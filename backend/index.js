ad13a810f71ed2f6a6e5fcfb0909ced710c277a6
Daniel S. Waite <daniel.s.waite@aexp.com>
2023-04-29 12:20:38 -0700

    initial setup

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const data = require('./data')
+const app = express()
+const port = 5000
+const cors = require("cors")
+app.use(cors({origin: "http://localhost:5000"}))
+
+app.get('/', (req, res) => {
+  res.send("Installation 00: Hi Mom, look it's express!")
+})
+
+app.get('/ring', (req, res) => {
+  res.send(data.rings)
+})
+
+app.get('/test', (req, res) => {
+  res.send(data.test)
+})
+
+app.listen(port, () => {
+  console.log(`Installation00 is running on port ${port}.`)
+})
