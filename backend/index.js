4466694bbc2489fd9a6d9a0784269db7a9533efa
Daniel Waite <Daniel.S.Waite@aexp.com>
2023-06-02 15:20:01 -0700

    setUser function & creat user functionality

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,16 @@ const app = express()
 const port = 5000
 const cors = require("cors")
 app.use(cors({origin: "http://localhost:3000"}))
-// const bcrypt = require('bcrypt');
+
+//token & request body
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+app.use(express.json());
+
+//encription
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env' });
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const setUser = async (req, res, next) => {
   if(!req.header('Authorization'))
@@ -17,31 +26,60 @@ const setUser = async (req, res, next) => {
   }
 }
 
+//TODO Write this function with const
+function creatUser(username, password) {
+  this.username = username;
+  this.password = password;
+}
+
+function findUser(username){
+  for (let i = 0; i < data.users.length; i++){
+    if (data.users[i].username == username)
+      return data.users[i]
+  }
+  return false
+}
+
 app.post('/register/', async (req, res) =>{
   const {username, password} = req.body;
   const hashedPw = await bcrypt.hash(password, 10);
-  const user = await User.create({username, password: hashedPw});
-  res.status(201).send(user);
+  data.users.push(new creatUser(username, hashedPw))
+  res.status(201).send(data.users);
 })
 
+app.post('/user/login', async (req, res) => {
+  const username = req.body.username
+  const foundUser = findUser(username)
+  if(!foundUser){
+    res.status(401).send('User not found');
+    return
+  }
 
-
+  const isMatch = await bcrypt.compare(req.body.password, foundUser.password);
+  if(isMatch) {
+    const token = jwt.sign({id: foundUser.id}, JWT_SECRET);
+    res.status(200).send({
+      message: `successfully logged in user ${foundUser.username}`,
+      token: token});
+  } else 
+    res.status(401).send('Incorrect password');
+})
 
 
 
 
 app.get('/', (req, res) => {
-  res.send("Installation 00: Hi Mom, look it's express!")
+  res.send("Final Elective Project: Hi Mom, look it's express!")
 })
 
-app.get('/ring', (req, res) => {
+app.get('/ring', setUser,(req, res) => {
   res.send(data.rings)
 })
 
-app.get('/test', (req, res) => {
-  res.send(data.test)
+app.get('/users', (req, res) => {
+  res.send(data.users)
 })
 
 app.listen(port, () => {
-  console.log(`Installation00 is running on port ${port}.`)
+  console.log(`Final Elective Project is running on port ${port}.`)
 })
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,31 +2,6 @@ import React from "react";
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
 
-
-// function Home() {
-//   return (
-//     <header style={{ paddingLeft: 0 }}>
-//       <div
-//         className='p-5 text-center bg-image'
-//         style={{ backgroundImage: "url('https://mdbootstrap.com/img/new/slides/041.webp')", height: 400 }}
-//       >
-//         <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
-//           <div className='d-flex justify-content-center align-items-center h-100'>
-//             <div className='text-white'>
-//               <h1 className='mb-3'>Heading</h1>
-//               <h4 className='mb-3'>Subheading</h4>
-//               <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
-//                 Call to action
-//               </a>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// }
-
-
 function Home() {
   return (
     <React.Fragment>
@@ -63,17 +38,14 @@ function Home() {
               <h1 class="forged" id="forg_four">Not exactly forged in the heart of a dying star, </h1>
               <h1 class="forged" id="forg_five">but forged from the raw materials of planetoids</h1>
             </div>
-            {/* <div id="learn"><a href="/about"><p id="learn_p">Learn How Halo's Are Born</p></a></div> */}
+            
             <div id="d_learn">
-              <Button id="learn" href="/abour">Learn How Halo's Are Born</Button>
+              <Button id="learn" href="/about">Learn How Halo's Are Born</Button>
             </div>
             <Image src={require("../media/halo&ark.png")} class="imgTopic" alt="Halo Sentinel" width="100%"/>
         </div>
-
-
     </React.Fragment>
   );
 }
 
 export default Home;
-
